d7c1cc8ed4eb9bf2c9616f4d415f8e838408bab9
u5er1ame <lapd.ga@gmail.com>
2026-04-19 13:24:07 +0300

    feat: db root info from server

---
+++ src/lib/server/root_db.svelte.ts
@@ -0,0 +1,33 @@
+import { env } from "$env/dynamic/private"
+import { Surreal } from "surrealdb"
+
+export class RootDb {
+	url = $state(new URL(env.SURREAL_URL))
+	_db = new Surreal()
+	isConnected = $state(false)
+	loadInfo = $state()
+	constructor() {
+	}
+	async connect() {
+		this.isConnected = await this._db.connect(this.url, {
+			authentication: {
+				// FIXME: change this!
+				username: "viewer",
+				password: "viewer",
+			}
+		}).catch(()=>this.isConnected = false);
+	}
+	async getInfo() {
+		/* system: {
+	  available_parallelism: 8,
+	  cpu_usage: 0,
+	  load_average: [Array],
+	  memory_allocated: 12780417,
+	  memory_usage: 151969792,
+	  physical_cores: 4
+	} */
+		if(!this.isConnected) return
+		const [res] = await this._db.query<[{system: any}]>("info for root");
+		this.loadInfo = res.system
+	}
+}
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,23 @@
 import type { PageServerData } from "./$types";
 import { getDbLocals } from "$lib/server/utils";
 
+import { RootDb } from "$lib/server/root_db.svelte";
 
+const db = new RootDb();
 export const load: PageServerData = async () => {
 	const locals = getDbLocals();
-
-	return { db: "", creds: locals };
+	await db.connect();
+	await db.getInfo();
+	const url = new URL(db.url)
+	url.protocol = "ws" // FIXME: secure later
+	url.username = "user"
+	url.password = "user"
+	return {
+		db: {
+			connected: db.isConnected,
+			info: db.loadInfo,
+			url
+		},
+		credentials: locals
+	};
 };
